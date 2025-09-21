977b4614fa72ac9a444ffdef75d8da77ba6bf8a5
FalahHadziqul <hadziqulfalah18@gmail.com>
2025-09-21 16:16:41 +0700

    Tugas 4 Code

--- main/views.py
+++ main/views.py
@@ -3,31 +3,82 @@ from main.models import Product
 from main.forms import ProductForm
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+@login_required(login_url='/login')
 def show_main(request):
-    product_list = Product.objects.all()
+    filter_type = request.GET.get("filter", "all") 
+
+    if filter_type == "all":
+        product_list = Product.objects.all()
+    else:
+        product_list = Product.objects.filter(user=request.user)
 
     context = {
         'shop_name' : 'Egoist Must Buy',
-        'npm' : '2406437432',
-        'name': 'Muhammad Hadziqul Falah Teguh',
+        'npm': '2406437432',
+        'name': request.user.username,
         'class': 'PBP E',
-        'product_list' : product_list
+        'product_list': product_list,
+        'last_login': request.COOKIES.get('last_login', 'Never')
     }
+    return render(request, "main.html",context)
 
-    return render(request, "main.html", context)
-
+@login_required(login_url='/login')
 def create_product(request):
     form = ProductForm(request.POST or None)
 
-    if form.is_valid() and request.method == "POST":
-        form.save()
+    if form.is_valid() and request.method == 'POST':
+        product_entry = form.save(commit = False)
+        product_entry.user = request.user
+        product_entry.save()
         return redirect('main:show_main')
 
-    context = {'form': form}
+    context = {
+        'form': form
+    }
+
     return render(request, "create_product.html", context)
 
+@login_required(login_url='/login')
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id)
     product.increment_views()
@@ -38,21 +89,25 @@ def show_product(request, id):
 
     return render(request, "product_detail.html", context)
 
+@login_required(login_url='/login')
 def show_xml(request):
     product_list = Product.objects.all()
     xml_data = serializers.serialize("xml", product_list)
     return HttpResponse(xml_data, content_type="application/xml")
 
+@login_required(login_url='/login')
 def show_json(request):
     product_list = Product.objects.all()
     json_data = serializers.serialize("json", product_list)
     return HttpResponse(json_data, content_type="application/json")
 
+@login_required(login_url='/login')
 def show_xml_by_id(request, product_id):
     product_item = get_object_or_404(Product, pk=product_id)
     xml_data = serializers.serialize("xml", [product_item])
     return HttpResponse(xml_data, content_type="application/xml")
 
+@login_required(login_url='/login')
 def show_json_by_id(request, product_id):
     product_item = get_object_or_404(Product, pk=product_id)
     json_data = serializers.serialize("json", [product_item])
